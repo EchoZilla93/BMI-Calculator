9248035e0f97dfebf2d4725da2f0b48595b8d3fd
Oleksandr Holovin <echozilla666@gmail.com>
2020-09-15 14:04:06 +0300

    Initial Commit

---
+++ BMI Calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BMI Calculator
+//
+//  Created by Александр Головин on 15.09.2020.
+//  Copyright © 2020 Александр Головин. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
