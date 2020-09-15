dc5495c3448a6f1ae67e5c85ce53da753ac85a01
Oleksandr Holovin <echozilla666@gmail.com>
2020-09-15 15:06:38 +0300

    assets,responsive UI

--- BMI Calculator/ViewController.swift
+++ BMI Calculator/ViewController.swift
@@ -9,10 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let backgroundImageView = UIImageView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        setBackground()
+    }
+    
+    func setBackground(){
+        self.view.addSubview(backgroundImageView)
+        backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
+        backgroundImageView.topAnchor.constraint(equalTo: view.topAnchor, constant:0).isActive = true
+        backgroundImageView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant:0).isActive = true
+        backgroundImageView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: 0).isActive = true
+        backgroundImageView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 0).isActive = true
+        backgroundImageView.image = UIImage(named:"calculate_background")
+        view.sendSubviewToBack(backgroundImageView)
     }
 
 
